922320180af09c63d38ad29c8c30f1c7bfd031da
nazekov <nazekov.ul@gmail.com>
2023-05-23 18:56:19 +0600

    change task1 Computer1

---
+++ src/com/example/task1/Computer.java
@@ -0,0 +1,14 @@
+package com.example.task1;
+
+public class Computer {
+
+    String name = "IBM";
+
+    void on() {
+        System.out.println(name +  " on");
+    }
+
+    void off() {
+        System.out.println(name +  " off");
+    }
+}
