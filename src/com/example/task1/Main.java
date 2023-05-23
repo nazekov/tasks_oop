e001086ce4b676b2f811b242f647425a839492c5
nazekov <nazekov.ul@gmail.com>
2023-05-23 18:51:03 +0600

    task1 Computer1

---
+++ src/com/example/task1/Main.java
@@ -0,0 +1,13 @@
+package com.example.task1;
+
+import com.example.task1.model.Computer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.on();
+        computer.off();
+
+    }
+}
