ebe6cf7b842d0292a60f1f5f410cd360fc5bcf41
nazekov <nazekov.ul@gmail.com>
2023-06-02 20:21:47 +0600

    task6 Printer used String.format

--- src/com/example/task6_printer/v1/Printer.java
+++ src/com/example/task6_printer/v1/Printer.java
@@ -18,22 +18,31 @@ public class Printer implements Printable {
 
     @Override
     public void print(String text) {
+        String result = null;
         int textSize = text.trim().replaceAll("\\s+", "").length();
         if (textSize > paintVolume) {
-            System.out.println("Не хватило краски: " + (textSize - paintVolume)
-                                            + " (всего краски в принтере " + paintVolume);
+//            System.out.println("Не хватило краски: " + (textSize - paintVolume)
+//                                            + " (всего краски в принтере " + paintVolume);
+            result = String.format("Не хватило краски: %d (всего краски в принтере %d)", textSize - paintVolume, paintVolume);
+            System.out.println(result);
         } else {
-            System.out.println("На печать отправлен текст: " + text + ", длина: "  + textSize);
+            result = String.format("На печать отправлен текст: %s, длина: %d", text, textSize);
+//            System.out.println("На печать отправлен текст: " + text + ", длина: "  + textSize);
+            System.out.println(result);
             paintVolume -= textSize;
-            System.out.println("Текст распечатан: " + text
-                                            + ", у принтера осталось краски: " + paintVolume);
+            result = String.format("Текст распечатан: %s, у принтера осталось краски: %d", text, paintVolume);
+//            System.out.println("Текст распечатан: " + text
+//                                            + ", у принтера осталось краски: " + paintVolume);
+            System.out.println(result);
         }
     }
 
     @Override
     public void charge(int volume) {
         paintVolume += volume;
-        System.out.println("Принтер пополнен на длину текста " + volume
-                        + " (всего краски в принтере: " + paintVolume + ")");
+        String result = String.format("Принтер пополнен на длину текста %d (всего краски в принтере: %d)", volume, paintVolume);
+//        System.out.println("Принтер пополнен на длину текста " + volume
+//                        + " (всего краски в принтере: " + paintVolume + ")");
+        System.out.println(result);
     }
 }
